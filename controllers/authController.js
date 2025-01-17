8cbd87ea5df298f766e91c417ba2ebfa61935dfd
Viitorapps2025 <viitorapps.tech@gmail.com>
2025-01-17 13:38:08 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,8 +13,6 @@ const transport = require('../middlewares/sendMail');
 
 
 <<<<<<< HEAD
-const crypto = require('crypto');
-const nodemailer = require('nodemailer');
 
 =======
 >>>>>>> f1455586ba7a2598b1356a17e0ea96b52541b4fe
